44f646a91b992402d5938b8c98f4d37da8e2d9f4
palitstock-hash <palitstock@gmail.com>
2026-04-26 01:48:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 1) ใส่ค่า Supabase ของคุณตรงนี้หลังสร้างโปรเจกต์
 const SUPABASE_URL = "https://mrakcyiyztqrldnmgemy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_nF4S11QqFyzJDSBl03zAJw_...";
+const SUPABASE_ANON_KEY = sb_publishable_JNIBifUx3oqIhLSaVFG_sw_fXkS_I15
 const hasSupabase = !SUPABASE_URL.includes('PASTE_');
 const supabaseClient = hasSupabase ? supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY) : null;
 
